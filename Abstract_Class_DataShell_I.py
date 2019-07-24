2bf7a26f71df7e9242e12196783bad027c5ec76f
RKiddle <RKiddle@users.noreply.github.com>
2019-07-24 21:59:04 +0700

    Create Abstract_Class_DataShell_I.py

---
+++ Abstract_Class_DataShell_I.py
@@ -0,0 +1,11 @@
+# Load numpy as np and pandas as pd
+import numpy as np
+import pandas as pd
+
+# Create class: DataShell
+class DataShell:
+    def __init__(self, inputFile):
+        self.file = inputFile
+
+# Instantiate DataShell as my_data_shell
+my_data_shell = DataShell(us_life_expectancy)
